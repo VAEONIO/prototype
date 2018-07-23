70693a4213b31c9b2aef725bd52a93da8a4a8ad2
Florian Gauger <flo.gauger@gmail.com>
2018-07-23 18:27:31 +0200

    Fix compile script

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -2,5 +2,6 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-mkdir $DIR/../build
-/usr/local/eosio/bin/eosiocpp -o $DIR/../build/profile.wast $DIR/../volean/profile.hpp $DIR/../volean/profile.cpp
+mkdir -p $DIR/../build
+/usr/local/eosio/bin/eosiocpp -g $DIR/../build/profile.abi $DIR/../volean/profile.hpp
+/usr/local/eosio/bin/eosiocpp -o $DIR/../build/profile.wast $DIR/../volean/profile.cpp

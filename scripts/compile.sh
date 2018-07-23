af3dcd4ae8fdec0aad4ba18ff5612941ecabb151
Florian Gauger <flo.gauger@gmail.com>
2018-07-23 20:19:26 +0200

    Add hack to build script

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -2,6 +2,14 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-mkdir -p $DIR/../build
-/usr/local/eosio/bin/eosiocpp -g $DIR/../build/profile.abi $DIR/../volean/profile.hpp
-/usr/local/eosio/bin/eosiocpp -o $DIR/../build/profile.wast $DIR/../volean/profile.cpp
+mkdir -p $DIR/../build/profile
+
+# hack because of buggy eosiocpp
+cp -Rf $DIR/../volean $DIR/../../eos/contracts
+cd $DIR/../../eos
+/usr/local/eosio/bin/eosiocpp -g $DIR/../build/profile/profile.abi contracts/volean/profile.hpp
+
+#/usr/local/eosio/bin/eosiocpp -g $DIR/../build/profile/profile.abi $DIR/../volean/profile.hpp
+/usr/local/eosio/bin/eosiocpp -o $DIR/../build/profile/profile.wast $DIR/../volean/profile.cpp
+
+/usr/local/eosio/bin/cleos set contract flo $DIR/../build/profile

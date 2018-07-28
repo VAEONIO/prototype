0dffd18287123245949bc3a8fae9b1e08b10c77c
Florian Gauger <flo.gauger@gmail.com>
2018-07-28 16:52:49 +0200

    Switch to form based input

--- src/public/main.js
+++ src/public/main.js
@@ -1,41 +1,113 @@
 var socket = io();
 
-var contracts = {
-  profile: {
-    name: "profile",
-    account: "vol.profile",
-    actions: {
-      create: {
-        command: "flo Flo GG flo@active"
-      },
-      update: {
-        command: "flo Florian G flo@active"
-      },
-      remove: {
-        command: "flo flo@active"
-      }
-    }
+class Argument {
+  constructor(name, value) {
+    this.name = name;
+    this.value = value;
   }
-};
+}
+
+class Action {
+  constructor(contract, contractAccount, name, args, auth) {
+    this.contract = contract;
+    this.contractAccount = contractAccount;
+    this.name = name;
+    this.args = args;
+    this.auth = auth;
+  }
+}
+
+var actions = [
+  new Action(
+    "profile",
+    "vol.profile",
+    "create",
+    [
+      new Argument("account_name", "flo"),
+      new Argument("first_name", "Flo"),
+      new Argument("last_name", "GG")
+    ],
+    "flo@active"
+  ),
+  new Action(
+    "profile",
+    "vol.profile",
+    "update",
+    [
+      new Argument("account_name", "flo"),
+      new Argument("first_name", "Florian"),
+      new Argument("last_name", "G")
+    ],
+    "flo@active"
+  ),
+  new Action(
+    "profile",
+    "vol.profile",
+    "remove",
+    [new Argument("account_name", "flo")],
+    "flo@active"
+  )
+];
+//profile: {
+//  name: "token",
+//  account: "vol.token",
+//  actions: {
+//    create: {
+//      command: "vol.token 100000 VOL vol.token@active"
+//    },
+//    issue: {
+//      command: "flo 1000 VOL flo@active"
+//    },
+//    remove: {
+//      command: "flo flo@active"
+//    }
+//  }
+//}
 
 $(document).ready(function() {
-  for (var c_key in contracts) {
-    var c = contracts[c_key];
-    for (var a_key in c.actions) {
-      var a = c.actions[a_key];
-      $option = $("<option></option>")
-        .val(c.account + " " + a_key + " " + a.command)
-        .text(c.name + " " + a_key);
-      $("#actions").append($option);
-    }
+  for (let i = 0; i < actions.length; i++) {
+    let action = actions[i];
+    $option = $("<option></option>")
+      .text(action.contract + " " + action.name)
+      .data("action", action);
+    $("#actions").append($option);
+  }
+
+  function setCmd() {
+    const action = $("#actions")
+      .find(":selected")
+      .data("action");
+
+    let cmd = action.contractAccount + " " + action.name;
+    $("#actionInputContainer > input").each(function() {
+      cmd += " " + $(this).val();
+    });
+    $("#cmd").text(cmd);
+  }
+
+  function createActionFieldInput(name, value) {
+    const id = "actionInput_" + name;
+    const $label = $("<label></label>")
+      .text(name)
+      .attr("for", id);
+    const $input = $("<input  class='u-full-width' type='text'></input>")
+      .val(value)
+      .attr("id", id);
+    $input.on("input", setCmd);
+    $("#actionInputContainer").append($label);
+    $("#actionInputContainer").append($input);
+    setCmd();
   }
 
   function updateAction() {
-    $("#actionInput").val(
-      $("#actions")
-        .find(":selected")
-        .val()
-    );
+    $("#actionInputContainer").empty();
+    const action = $("#actions")
+      .find(":selected")
+      .data("action");
+    for (let i = 0; i < action.args.length; i++) {
+      createActionFieldInput(action.args[i].name, action.args[i].value);
+    }
+    createActionFieldInput("authentification", action.auth);
   }
 
   $("#actions").change(updateAction);
@@ -43,14 +115,20 @@ $(document).ready(function() {
 
   $("#action").submit(function() {
     $("#error").text("");
-    var input = $("#actionInput")
-      .val()
-      .split(" ");
+
+    const action = $("#actions")
+      .find(":selected")
+      .data("action");
+
+    var args = [];
+    $("#actionInputContainer > input").each(function() {
+      args.push($(this).val());
+    });
     var data = {
-      contract: input[0],
-      action: input[1],
-      args: input.slice(2, -1),
-      auth: input[input.length - 1]
+      contract: action.contractAccount,
+      action: action.name,
+      args: args.slice(0, -1),
+      auth: args[args.length - 1]
     };
     console.log(data);
     socket.emit("action", data);

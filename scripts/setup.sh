0c29ec0a796ab9f5834b1dc521bf20d36cba4eb8
Florian Gauger <flo.gauger@gmail.com>
2018-08-04 16:59:32 +0200

    Refactor setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,36 +1,38 @@
 #!/usr/local/bin/bash
 
-# device to pipe output to
+eos_bin_dir=/usr/local/eosio/bin
+eos_dir=/Users/flo/projects/eos
+# devices to pipe output to
 keosd_output_device=/dev/ttys016
 nodeos_output_device=/dev/ttys018
 
+# setup commands
+keosd_server_address="localhost:8899"
+cleos="$eos_bin_dir/cleos --wallet-url=http://$keosd_server_address"
+keosd=$eos_bin_dir/keosd
+nodeos=$eos_bin_dir/nodeos
+eosiocpp=$eos_bin_dir/eosiocpp
+
+# setup directories
 dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
-
-source $dir/vars.sh
-
 runtime_dir=$dir/../runtime
 build_dir=$dir/../build
-
 rm -rf $runtime_dir
 rm -rf $build_dir
 mkdir -p $runtime_dir/wallets
 mkdir -p $build_dir
-
 $dir/create_config.sh $runtime_dir/config.ini $runtime_dir/wallets
-
 config_dir="--config-dir $runtime_dir"
 
-# start keosd
+# start keosd and nodeos
 pkill keosd
 $keosd --http-server-address=$keosd_server_address $config_dir >$keosd_output_device 2>&1 &
-
-# start nodeos
 pkill nodeos
 $nodeos -e -p eosio --plugin eosio::chain_api_plugin --plugin eosio::history_api_plugin $config_dir --delete-all-blocks >$nodeos_output_device 2>&1 &
-
 # wait for nodeos to start
-sleep 1
+sleep 2
 
+# setup accounts
 accounts=(flo andi vae.token vaeon vae.cash vae.fee)
 declare -A passwords
 declare -A private_keys
@@ -53,12 +55,12 @@ for account in "${accounts[@]}"; do
 	public_keys[$account]+=$public_key
 	echo "key created - " $private_key " - " $public_key
 
-	echo $private_key >>$build_dir/private_keys.txt
 	echo $account >>$build_dir/accounts.txt
+	echo $private_key >>$build_dir/private_keys.txt
 
 	$cleos wallet import -n $account --private-key $private_key >/dev/null
 
-	#import eosio private key
+	# import eosio private key
 	$cleos wallet import -n $account --private-key 5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3 >/dev/null
 
 	$cleos create account eosio $account $public_key $public_key >/dev/null
@@ -66,28 +68,27 @@ done
 
 $cleos wallet import -n vaeon --private-key ${private_keys[flo]} >/dev/null
 
+# setup permissions so that our contract can send tokens with SEND_INLINE_ACTION
 $cleos set account permission flo active '{"threshold": 1,"keys": [{"key": "'${public_keys[vaeon]}'","weight": 1}],"accounts": [{"permission":{"actor":"vaeon","permission":"eosio.code"},"weight":1}]}' owner -p flo@active
 $cleos set account permission andi active '{"threshold": 1,"keys": [{"key": "'${public_keys[vaeon]}'","weight": 1}],"accounts": [{"permission":{"actor":"vaeon","permission":"eosio.code"},"weight":1}]}' owner -p andi@active
 $cleos set account permission vae.cash active '{"threshold": 1,"keys": [{"key": "'${public_keys[vaeon]}'","weight": 1}],"accounts": [{"permission":{"actor":"vaeon","permission":"eosio.code"},"weight":1}]}' owner -p vae.cash@active
-
 $cleos set account permission vaeon active '{"threshold": 1,"keys": [{"key": "'${public_keys[vaeon]}'","weight": 1}],"accounts": [{"permission":{"actor":"vaeon","permission":"eosio.code"},"weight":1}]}' owner -p vaeon@active
 
-#vaeon contract
+# compile and set vaeon contract
 mkdir -p $build_dir/vaeon
-
 # hack because of buggy eosiocpp
-cp -Rf $dir/../contracts $dir/../../eos/contracts
-cd $dir/../../eos
+cp -Rf $dir/../contracts $eos_dir/contracts
+cd $eos_dir
 $eosiocpp -g $build_dir/vaeon/vaeon.abi contracts/contracts/vaeon.hpp
-
-#/usr/local/eosio/bin/eosiocpp -g $dir/../build/vaeon/vaeon.abi $dir/../contracts/vaeon.hpp
+#$eosiocpp -g $dir/../build/vaeon/vaeon.abi $dir/../contracts/vaeon.hpp
 $eosiocpp -o $build_dir/vaeon/vaeon.wast contracts/contracts/vaeon.cpp
-
 $cleos set contract vaeon $build_dir/vaeon
 
-#token contract
-$cleos set contract vae.token $dir/../../eos/build/contracts/eosio.token
+# set token contract
+$cleos set contract vae.token $eos_dir/build/contracts/eosio.token
 
+# create marker file, used to indiciate that default profiles need to be setup
 touch $build_dir/NEW
 
+# start webserver
 node $dir/../server/app.js
